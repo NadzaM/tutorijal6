f8f11668f66a16e231813b32d2df145d6a2da1eb
NadzaM <nmemic2@etf.unsa.ba>
2020-09-09 14:01:37 +0200

    initial commit

---
+++ tutorijal6/src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -0,0 +1,10 @@
+package ba.unsa.etf.rpr.tutorijal05;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class Controller {
+
+    public Label unosBtn;
+    public Button jednakoBtn;
+}
